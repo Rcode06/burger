5139cd7ec48072cecf1786b07c3007bc8b6a70a9
reyna <reyna@reynas-MacBook-Pro.local>
2017-10-26 20:39:48 -0700

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mexican Avocado Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey-Panela Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Regular Cheeseburger', true);
+
+
+
---
+++ models/burger.js
@@ -0,0 +1,22 @@
+var orm = require('../config/orm.js');
+
+var burger = {
+	all: function(cb){
+		orm.all('burgers', function(res){
+			cb(res);
+		});
+	},
+	create: function(col, vals, cb){
+		orm.create('burgers', col, vals, function(res){
+			cb(res);
+		});
+	},
+	update: function(objColVals, condition, cb){
+		orm.update('burgers', objColVals, condition, function(res){
+			cb(res);
+		});
+	}	
+
+};
+// Export database functions
+module.exports = burger;
